2799998ccb30a7564bdb5a552a0627d17ebdeeea
keepprogress <keepprogress@gmail.com>
2023-04-16 13:43:15 +0800

    Sec24. 346. Building a First Standalone Component.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+// import { DetailsComponent } from "./details/details.component";
 
 @Component({
+  // import: [DetailsComponent],
   selector: 'app-welcome',
   templateUrl: './welcome.component.html'
 })
